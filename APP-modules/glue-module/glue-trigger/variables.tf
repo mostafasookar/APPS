502540471668bd46a1baa59e1e87bda380e481ef
Moustafa Mohamed Sokkar <mostafasookar@hotmail.com>
2025-10-25 20:47:10 +0400

    add glue-module

---
+++ APP-modules/glue-module/glue-trigger/variables.tf
@@ -0,0 +1,7 @@
+variable "scripts_bucket" {}
+variable "role_arn" {}
+variable "glue_version" {}
+variable "python_version" {}
+variable "command_name" {}
+variable "default_arguments" {}
+variable "tags" {}
